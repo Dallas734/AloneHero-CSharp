cf470e673ed2a55b0dbe7cb17d16689efb5b889a
Mark <vernidub66@gmail.com>
2023-02-03 14:43:44 +0300

    Menu!

--- AloneHero-CSharp/Game.cs
+++ AloneHero-CSharp/Game.cs
@@ -17,79 +17,105 @@ namespace AloneHero_CSharp
         private Level level;
         private List<Level> levels;
         private int curLevel;
-        private int prevLevel;
+
+        private RenderWindow MENU;
+        private Menu mainMenu;
 
         public Game()
         {
             endGame = false;
             curLevel = 0;
+            
         }
 
         public bool StartGame()
         {
-            RenderWindow window = new RenderWindow(new VideoMode(1200, 800), "Alone Hero");
-            Clock clock = new Clock();
+            MENU = new RenderWindow(new VideoMode(1200, 800), "Alone Hero");
+            mainMenu = new Menu(MENU.Size.X, MENU.Size.Y);
+            //Clock clock = new Clock();
 
-            // Инициализация уровней (на будущее)
-            levels = new List<Level>();
-            levels.Add(new Level("map_XML_2.tmx", this));
-            levels.Add(new Level("map_XML_1.tmx", this));
+            //// Инициализация уровней (на будущее)
+            //levels = new List<Level>();
+            //levels.Add(new Level("map_XML_2.tmx", this));
+            //levels.Add(new Level("map_XML_1.tmx", this));
 
-            level = levels[curLevel];
+            //level = levels[curLevel];
 
-            // Подписка на событие
-            foreach (Level level1 in levels)
+            //// Подписка на событие
+            //foreach (Level level1 in levels)
+            //{
+            //    level1.EndGame += GetMessageEventHandler;
+            //    level1.NextLevel += GetMessageEventHandler;
+            //}
+
+            //    // Интерфейс
+            //GameInterface gameInterface = new GameInterface(this);
+
+            MENU.KeyPressed += Window_KeyPressed;
+
+            while (MENU.IsOpen)
             {
-                level1.EndGame += GetMessageEventHandler;
-                level1.NextLevel += GetMessageEventHandler;
+                // Время для анимации
+                //float time = clock.ElapsedTime.AsMicroseconds();
+                //clock.Restart();
+                //time = time / 800;
+                
+
+                MENU.DispatchEvents();
+                mainMenu.Draw(MENU);
+
+                //if (Keyboard.IsKeyPressed(Keyboard.Key.Tab) || endGame)
+                //{
+                //    window.Close();
+                //    return true;
+                //}
+                //if (Keyboard.IsKeyPressed(Keyboard.Key.Escape)) return false;
+
+
+                //if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+                //{
+                //    mainMenu.MoveUp();
+                //}
+                //if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+                //{
+                //    mainMenu.MoveDown();
+                //}
+                //if (Keyboard.IsKeyPressed(Keyboard.Key.Enter))
+                //{
+                //    RenderWindow PLAY = new RenderWindow(new VideoMode(1200, 800), "Alone Hero");
+
+                //}
+
+
+                // Здесь вставить отрисовку меню 
+
+                //level.Draw(window, time);
+                //LoadGame?.Invoke(this, new OrderEventArgs(Codes.HEALTH_UNITS, level.GetPlayer().Health, gameInterface));
+                //LoadGame?.Invoke(this, new OrderEventArgs(Codes.SPEED_UNITS, level.GetPlayer().Speed, gameInterface));
+                //LoadGame?.Invoke(this, new OrderEventArgs(Codes.COIN_UNITS, level.GetPlayer().Coins, gameInterface));
+                ////Message healthUnits = new Message(Codes.HEALTH_UNITS, levels[0].GetPlayer().Health, null);
+                ////Message speedUnits = new Message(Codes.SPEED_UNITS, levels[0].GetPlayer().Speed, null);
+                //if (level.GetPlayer().Speed > 0.1)
+                //{
+                //    int a = 0;
+                //}
+                ////gameInterface.GetMessage(healthUnits);
+                ////gameInterface.GetMessage(speedUnits);
+                //gameInterface.Draw(window);
+                // Отображение нарисованного
+                MENU.Display();
             }
-
-                // Интерфейс
-                GameInterface gameInterface = new GameInterface(this);
-
-                window.KeyPressed += Window_KeyPressed;
-
-                while (window.IsOpen)
-                {
-                    // Время для анимации
-                    float time = clock.ElapsedTime.AsMicroseconds();
-                    clock.Restart();
-                    time = time / 800;
-
-                    window.DispatchEvents();
-
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.Tab) || endGame)
-                    {
-                        window.Close();
-                        return true;
-                    }
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.Escape)) return false;
-
-                    level.Draw(window, time);
-                    LoadGame?.Invoke(this, new OrderEventArgs(Codes.HEALTH_UNITS, level.GetPlayer().Health, gameInterface));
-                    LoadGame?.Invoke(this, new OrderEventArgs(Codes.SPEED_UNITS, level.GetPlayer().Speed, gameInterface));
-                    LoadGame?.Invoke(this, new OrderEventArgs(Codes.COIN_UNITS, (int)level.GetPlayer().Coins, gameInterface));
-                    //Message healthUnits = new Message(Codes.HEALTH_UNITS, levels[0].GetPlayer().Health, null);
-                    //Message speedUnits = new Message(Codes.SPEED_UNITS, levels[0].GetPlayer().Speed, null);
-                    if (level.GetPlayer().Speed > 0.1)
-                    {
-                        int a = 0;
-                    }
-                    //gameInterface.GetMessage(healthUnits);
-                    //gameInterface.GetMessage(speedUnits);
-                    gameInterface.Draw(window);
-                    // Отображение нарисованного
-                    window.Display();
-                }
             
             return false;
+
         }
 
         public void GameRunning()
         {
-            if (StartGame())
+            if (/*StartGame()*/ endGame == false)
             {
-                endGame = false;
+                StartGame();
+                //endGame = false;
                 GameRunning();
             }
         }
@@ -117,10 +143,104 @@ namespace AloneHero_CSharp
 
         private void Window_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
         {
-            var window = (SFML.Window.Window)sender;
-            if (e.Code == SFML.Window.Keyboard.Key.Escape)
+            var window = (Window)sender;
+            if (e.Code == Keyboard.Key.Escape)
             {
                 window.Close();
+            }
+            if (e.Code == Keyboard.Key.Tab || endGame)
+            {
+                window.Close();
+                endGame = false;
+                
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+            {
+                mainMenu.MoveUp();
+                return;
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+            {
+                mainMenu.MoveDown();
+                return;
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Escape)) endGame = true;
+            
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Enter))
+            {
+               
+                int x = mainMenu.MenuNum;
+
+                if (x == 0)
+                {
+                    RenderWindow PLAY = new RenderWindow(new VideoMode(1200, 800), "Alone Hero");
+                    //window.Close();
+
+                    Clock clock = new Clock();
+
+                    // Инициализация уровней (на будущее)
+                    levels = new List<Level>();
+                    levels.Add(new Level("map_XML_2.tmx", this));
+                    levels.Add(new Level("map_XML_1.tmx", this));
+
+                    level = levels[curLevel];
+
+                    // Подписка на событие
+                    foreach (Level level1 in levels)
+                    {
+                        level1.EndGame += GetMessageEventHandler;
+                        level1.NextLevel += GetMessageEventHandler;
+                    }
+
+                    // Интерфейс
+                    GameInterface gameInterface = new GameInterface(this);
+
+                    PLAY.KeyPressed += Window_KeyPressed;
+
+                    while (PLAY.IsOpen)
+                    {
+                        // Время для анимации
+                        float time = clock.ElapsedTime.AsMicroseconds();
+                        clock.Restart();
+                        time = time / 800;
+
+                        PLAY.DispatchEvents();
+
+                        level.Draw(PLAY, time);
+                        LoadGame?.Invoke(this, new OrderEventArgs(Codes.HEALTH_UNITS, level.GetPlayer().Health, gameInterface));
+                        LoadGame?.Invoke(this, new OrderEventArgs(Codes.SPEED_UNITS, level.GetPlayer().Speed, gameInterface));
+                        LoadGame?.Invoke(this, new OrderEventArgs(Codes.COIN_UNITS, level.GetPlayer().Coins, gameInterface));
+                        //Message healthUnits = new Message(Codes.HEALTH_UNITS, levels[0].GetPlayer().Health, null);
+                        //Message speedUnits = new Message(Codes.SPEED_UNITS, levels[0].GetPlayer().Speed, null);
+                        if (level.GetPlayer().Speed > 0.1)
+                        {
+                            int a = 0;
+                        }
+                        //gameInterface.GetMessage(healthUnits);
+                        //gameInterface.GetMessage(speedUnits);
+                        gameInterface.Draw(PLAY);
+                        // Отображение нарисованного
+                        PLAY.Display();
+                    }
+                } 
+
+                if (x == 1)
+                {
+                    RenderWindow ABOUT = new RenderWindow(new VideoMode(1200, 800), "About");
+
+                    ABOUT.KeyPressed += Window_KeyPressed;
+                    while(ABOUT.IsOpen)
+                    {
+                        ABOUT.DispatchEvents();
+                        ABOUT.Display();
+                    }
+                }
+
+                if (x == 2)
+                {
+                    window.Close();
+                }
+
             }
         }
 
---
+++ AloneHero-CSharp/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Graphics;
+using SFML.System;
+
+namespace AloneHero_CSharp
+{
+    class Menu
+    {
+        private Font font;
+        private List<Text> mainMenu;
+        private bool isMenu;
+        public int MenuNum { get; private set; }
+
+        public Menu(float width, float height)
+        {
+            MenuNum = 0;
+            font = new Font("timesnewromanpsmt.ttf");
+            mainMenu = new List<Text>();
+            mainMenu.Add(new Text("Play", font, 70));
+            mainMenu[0].Position = new Vector2f(400, 200);
+
+            mainMenu.Add(new Text("About", font, 70));
+            mainMenu[1].Position = new Vector2f(400, 300);
+
+            mainMenu.Add(new Text("Exit", font, 70));
+            mainMenu[2].Position = new Vector2f(400, 400);
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            
+            foreach (Text element in mainMenu)
+            {
+                window.Draw(element);
+            }
+        }
+
+        public void MoveUp()
+        {
+            if (MenuNum >= 0)
+            {
+                mainMenu[MenuNum].FillColor = Color.White;
+
+                MenuNum--;
+                if (MenuNum == -1)
+                {
+                    MenuNum = 2;
+                }
+                mainMenu[MenuNum].FillColor = Color.Blue;
+            }
+        }
+
+        public void MoveDown()
+        {
+            if (MenuNum <= mainMenu.Count)
+            {
+                mainMenu[MenuNum].FillColor = Color.White;
+
+                MenuNum++;
+                if (MenuNum == mainMenu.Count)
+                {
+                    MenuNum = 0;
+                }
+
+                mainMenu[MenuNum].FillColor = Color.Blue;
+            }
+        }
+
+    }
+}
